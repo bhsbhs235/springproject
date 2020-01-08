a405f84d2257b09d01b34f954a0b4b7ba081015e
bhsbhs235 <bhsbhs235@naver.com>
2020-01-08 11:17:16 +0900

    docker-compose blue green test1

--- deploy.sh
+++ deploy.sh
@@ -1,43 +1,21 @@
 #!/bin/bash
-# bash Call
-# sudo bash {PATH}/deploy.sh 8080 springproject
- 
-# Field
- 
-# Server Port
-# Ex) 8080
-SERVER_PORT=$1
-# Service Name
-# Ex) springproject
-PROJECT_NAME=$2
- 
-PROJECT_PATH=/var/lib/jenkins/workspace/$PROJECT_NAME/build/libs
-WAR_FILE=$PROJECT_PATH/$PROJECT_NAME-0.0.1-SNAPSHOT.war
-TMP_PATH_NAME=/tmp/$PROJECT_NAME-pid
- 
-# Function
-function stop(){
-    sudo echo " "
-    sudo echo "Stoping process on port: $SERVER_PORT"
-    sudo fuser -n tcp -k $SERVER_PORT # tcp $serverPort에 해당하는 port를 Kill함.
- 
-    if [ -f $TMP_PATH_NAME ]; then
-        sudo rm $TMP_PATH_NAME
-    fi
- 
-    sudo echo " "
-}
- 
-function start(){
-    sudo echo " "
-    sudo nohup java -jar -Dserver.port=$SERVER_PORT $WAR_FILE /tmp 2>> /dev/null >> /dev/null & 
-    sudo echo "java -jar -Dserver.port=$SERVER_PORT $WAR_FILE /tmp 2>> /dev/null >> /dev/null &"
- 
-    sudo echo $! > $TMP_PATH_NAME
-    sudo echo " "
-}
- 
-# Function Call
-stop
- 
-start
+
+DOCKER_APP_NAME=springproject
+
+EXIST_BLUE=$(/usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+
+if [ -z "$EXIST_BLUE" ]; then
+    echo "blue up"
+    /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
+
+    sleep 10
+
+    /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+else
+    echo "green up"
+    /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
+
+    sleep 10
+
+    /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+fi
