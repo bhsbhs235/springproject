1c916df36193d4db1467752a5ebacd324aee0cf1
bhsbhs235 <bhsbhs235@naver.com>
2020-01-13 14:04:19 +0900

    nginx,springboot container test27

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ DOCKER_APP_NAME=springproject
 
 EXIST_BLUE=$(/usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
+/usr/local/bin/docker-compose  -p nginx -f docker-compose.nginx.yml down
+
+/usr/local/bin/docker-compose  -p nginx -f docker-compose.nginx.yml up -d
+
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
     /usr/local/bin/docker-compose  -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
