0aac7272df3a69cbc9849ee57cf27cfb870612f6
bhsbhs235 <bhsbhs235@naver.com>
2020-01-16 14:15:32 +0900

    nginx containerizing success!

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,14 @@ EXIST_BLUE=$(/usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-
 
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
-    /usr/local/bin/docker-compose  -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
+    /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
 
     sleep 10
 
     /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 else
     echo "green up"
-    /usr/local/bin/docker-compose  -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
+    /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
     sleep 10
 
